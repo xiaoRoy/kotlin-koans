a10776c55d65f2ae175285adfd108c5dd757ac6b
Jerry Li <xiaoroyg@163.com>
2018-02-19 23:20:30 +0800

    Finished task 26

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -16,4 +16,4 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(override val start: MyDate, override val endInclusive: MyDate):ClosedRange<MyDate>
