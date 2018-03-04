7523e786e9c6279aee4131160143b7c2f88c5a90
Jerry Li <xiaoroyg@163.com>
2018-03-04 15:03:32 +0800

    Finished task 29.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -10,11 +10,24 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
+operator fun MyDate.plus(interval: TimeInterval): MyDate{
+    return this.addTimeIntervals(interval, 1)
+}
+
+operator fun MyDate.plus(intervals: RepeatTimeIntervals): MyDate{
+    return this.addTimeIntervals(intervals.timeInterval, intervals.times)
+}
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+
+class RepeatTimeIntervals(val timeInterval: TimeInterval, val times: Int)
+
+operator fun TimeInterval.times(times: Int): RepeatTimeIntervals = RepeatTimeIntervals(this, times)
+
 
 class DateRange(override val start: MyDate, override val endInclusive: MyDate):ClosedRange<MyDate>{
 
